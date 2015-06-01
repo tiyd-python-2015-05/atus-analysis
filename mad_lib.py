e26bcc6b05b63ad52c54ea17fae6076f55ec4f7c
John Waldrep <johnwaldrep@gmail.com>
2015-06-01 07:11:36 -0400

    Add extra discussion and mad_lib example, updated README

--- mad_lib.py
+++ mad_lib.py
@@ -8,43 +8,44 @@ persons = ['Jack', 'Jill', 'Rocky', 'Dale', 'Urkel', 'Sharona', 'Mr. Noodle', 'D
 places = ['home', 'work', 'the gym', 'the pool', 'the coffeeshop', "your mom's house"]
 whens = ['earlier', 'today', 'yesterday', 'a while back', 'last night', 'the other day', 'long ago', 'in his/her prime', 'back when it meant something']
 
-activity = random.choice(list(activities.values()))
-lib = {'person': random.choice(persons),
-       'verb' : activity.lower(),
-       'place': random.choice(places),
-       'when' : random.choice(whens),
-       }
-starts_with_gerund = re.match(first_gerund, lib['verb'])
-if starts_with_gerund:
-    lib['verb'] = 'was really enjoying ' + lib['verb']
-else:
-    lib['verb'] = 'did a bit of ' + lib['verb']
+def mad_lib():
+    activity = random.choice(list(activities.values()))
+    lib = {'person': random.choice(persons),
+           'verb' : activity.lower(),
+           'place': random.choice(places),
+           'when' : random.choice(whens),
+           }
+    starts_with_gerund = re.match(first_gerund, lib['verb'])
+    if starts_with_gerund:
+        lib['verb'] = 'was really enjoying ' + lib['verb']
+    else:
+        lib['verb'] = 'did a bit of ' + lib['verb']
 
-print('{person} {verb} at {place} {when}.'.format(**lib))
+    print('{person} {verb} at {place} {when}.'.format(**lib))
 
-activity = random.choice(list(activities.values()))
-lib = {'person': random.choice(persons),
-       'verb' : activity.lower(),
-       'place': random.choice(places),
-       'when' : random.choice(whens),
-       }
-if starts_with_gerund:
-    lib['verb'] = 'ended up ' + lib['verb']
-else:
-    lib['verb'] = 'randomly decided to start doing ' + lib['verb']
+    activity = random.choice(list(activities.values()))
+    lib = {'person': random.choice(persons),
+           'verb' : activity.lower(),
+           'place': random.choice(places),
+           'when' : random.choice(whens),
+           }
+    if starts_with_gerund:
+        lib['verb'] = 'ended up ' + lib['verb']
+    else:
+        lib['verb'] = 'randomly decided to start doing ' + lib['verb']
 
-print('It was all fun and games until {person} {verb} all the time {when}.'.format(**lib))
+    print('It was all fun and games until {person} {verb} all the time {when}.'.format(**lib))
 
-activity = random.choice(list(activities.values()))
-lib = {'person': random.choice(persons),
-       'verb' : activity.lower(),
-       'place': random.choice(places),
-       'when' : random.choice(whens),
-       }
+    activity = random.choice(list(activities.values()))
+    lib = {'person': random.choice(persons),
+           'verb' : activity.lower(),
+           'place': random.choice(places),
+           'when' : random.choice(whens),
+           }
 
-# if starts_with_gerund:
-#     lib['verb'] = 'ended up ' + lib['verb']
-# else:
-#     lib['verb'] = 'randomly decided to start doing ' + lib['verb']
+    # if starts_with_gerund:
+    #     lib['verb'] = 'ended up ' + lib['verb']
+    # else:
+    #     lib['verb'] = 'randomly decided to start doing ' + lib['verb']
 
-print('{person} ultimately decided that {verb} just wasn\'t the same as it used to be {when}.'.format(**lib))
+    print('{person} ultimately decided that {verb} just wasn\'t the same as it used to be {when}.'.format(**lib))
